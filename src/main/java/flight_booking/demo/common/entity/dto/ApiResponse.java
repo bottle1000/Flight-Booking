d829a2baf45b61dfd4606d6f95fac7519f5fbfb7
thdwnsdlf61 <il4500@naver.com>
2025-02-14 22:04:44 +0900

    refactor : DiscountController 반환타입 수정, ApiResponse 주석처리
    - dev브랜치와 merge후 컨벤션 맞추기
    - dev브랜치의 ApiResponse와 겹치는 클래스 주석처리함

--- src/main/java/flight_booking/demo/common/entity/dto/ApiResponse.java
+++ src/main/java/flight_booking/demo/common/entity/dto/ApiResponse.java
@@ -1,12 +1,12 @@
-package flight_booking.demo.common.entity.dto;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
-public class ApiResponse<T> {
-    private String status;
-    private String message;
-    private T data;
-}
+//package flight_booking.demo.common.entity.dto;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//
+//@Getter
+//@AllArgsConstructor
+//public class ApiResponse<T> {
+//    private String status;
+//    private String message;
+//    private T data;
+//}
--- src/main/java/flight_booking/demo/domain/discount/controller/DiscountController.java
+++ src/main/java/flight_booking/demo/domain/discount/controller/DiscountController.java
@@ -1,6 +1,6 @@
 package flight_booking.demo.domain.discount.controller;
 
-import flight_booking.demo.common.entity.dto.ApiResponse;
+import flight_booking.demo.common.ApiResponse;
 import flight_booking.demo.domain.discount.dto.request.DiscountCreateRequest;
 import flight_booking.demo.domain.discount.dto.request.DiscountEndAtUpdateRequest;
 import flight_booking.demo.domain.discount.dto.response.DiscountCreateResponse;
@@ -28,11 +28,9 @@ public class DiscountController {
         //Todo: 변수명과 메서드명 겹침 -> 어떻게 바꾸지..?
         DiscountCreateResponse createdEvent = discountService.createEvent(request);
 
-        return ResponseEntity.ok(new ApiResponse<>(
-                HttpStatus.CREATED.toString(),
-                "할인 이벤트 생성 성공",
-                createdEvent
-        ));
+        return ResponseEntity.ok(ApiResponse
+                .success("할인 이벤트 생성 성공", createdEvent)
+        );
     }
 
     // Todo: discount_id or discountId -> 뭐가 맞을까...?
@@ -44,11 +42,9 @@ public class DiscountController {
 
         DiscountEndAtResponse updatedEventDate = discountService.updateEndAt(discount_id, request);
 
-        return ResponseEntity.ok(new ApiResponse<>(
-                HttpStatus.OK.toString(),
-                "할인 종료일 수정 성공",
-                updatedEventDate
-        ));
+        return ResponseEntity.ok(ApiResponse
+                .success("할인 종료일 수정 성공", updatedEventDate)
+        );
     }
 
     @GetMapping
@@ -56,9 +52,8 @@ public class DiscountController {
 
         List<DiscountListResponse> eventToList = discountService.findEventToList();
 
-        return ResponseEntity.ok(new ApiResponse<>(
-                HttpStatus.OK.toString(),
-                "할인 목록 조회 성공",
-                eventToList));
+        return ResponseEntity.ok(ApiResponse.success("할인 목록 조회 성공",
+                eventToList)
+        );
     }
 }
