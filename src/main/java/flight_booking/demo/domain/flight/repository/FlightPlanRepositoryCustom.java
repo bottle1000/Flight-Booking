1290bd11a8930d594d3b1f20f47f8eab92fae623
sinwoo-kim <shinwoo5676@gmail.com>
2025-02-14 15:03:46 +0900

    fix: FlightPlan CR 리팩토링 - 1. ModelAttribute 사용 - 2. createFlightPlan API location 제거

--- src/main/java/flight_booking/demo/domain/flight/repository/FlightPlanRepositoryCustom.java
+++ src/main/java/flight_booking/demo/domain/flight/repository/FlightPlanRepositoryCustom.java
@@ -5,12 +5,13 @@ import java.time.LocalDateTime;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import flight_booking.demo.domain.flight.entity.Airport;
 import flight_booking.demo.domain.flight.entity.FlightPlan;
 
 public interface FlightPlanRepositoryCustom {
 	Page<FlightPlan> findByFilters(
-		String departure,
-		String arrival,
+		Airport departure,
+		Airport arrival,
 		LocalDateTime boardingAt,
 		LocalDateTime landingAt,
 		Pageable pageable
