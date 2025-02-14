f645a5460f838379242f581b0fabaf83dfa0cc94
박병천 <genius178896@gmail.com>
2025-02-14 20:11:28 +0900

    feat: Insert JsonType Of MetaData To Invoice

--- src/main/java/flight_booking/demo/domain/invoice/repository/InvoiceRepository.java
+++ src/main/java/flight_booking/demo/domain/invoice/repository/InvoiceRepository.java
@@ -1,6 +1,6 @@
-package flight_booking.demo.domain.receipt.repository;
+package flight_booking.demo.domain.invoice.repository;
 
-import flight_booking.demo.domain.receipt.entity.Invoice;
+import flight_booking.demo.domain.invoice.entity.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
