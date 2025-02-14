72b69a4158c51d45075bd7ba6dec92dc715ceaf9
sinwoo-kim <shinwoo5676@gmail.com>
2025-02-14 13:53:51 +0900

    feat: Airplane CR 구현

--- src/main/java/flight_booking/demo/domain/airplane/AirplaneController.java
+++ src/main/java/flight_booking/demo/domain/airplane/AirplaneController.java
@@ -1,13 +1,21 @@
 package flight_booking.demo.domain.airplane;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import flight_booking.demo.common.ApiResponse;
 import flight_booking.demo.domain.airplane.dto.request.AirplaneCreateRequest;
 import flight_booking.demo.domain.airplane.dto.response.AirplaneCreateResponse;
+import flight_booking.demo.domain.airplane.dto.response.AirplaneGetResponse;
 import flight_booking.demo.domain.airplane.entity.Airplane;
 import flight_booking.demo.domain.airplane.repository.AirplaneRepository;
 import jakarta.validation.Valid;
@@ -18,13 +26,19 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/airplanes")
 public class AirplaneController {
 
-	private final AirplaneRepository airplaneRepository;
+	private final AirplaneService airplaneService;
 
 	@PostMapping
-	public ResponseEntity<AirplaneCreateResponse> createAirplane(@Valid  @RequestBody AirplaneCreateRequest request) {
-		Airplane airplane = Airplane.create(request.name());
-		Airplane savedAirplane = airplaneRepository.save(airplane);
-		AirplaneCreateResponse response = AirplaneCreateResponse.from(savedAirplane);
-		return ResponseEntity.ok().body(response);
+	public ResponseEntity<ApiResponse<AirplaneCreateResponse>> createAirplane(@Valid  @RequestBody AirplaneCreateRequest request) {
+		AirplaneCreateResponse response = airplaneService.createAirplane(request);
+		return ResponseEntity.ok(ApiResponse.success("항공기가 성공적으로 등록되었습니다.", response));
+	}
+
+	@GetMapping
+	public ResponseEntity<ApiResponse<Page<AirplaneGetResponse>>> getAirplaneList(
+		@PageableDefault(size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable
+	) {
+		Page<AirplaneGetResponse> response = airplaneService.getAirplaneList(pageable);
+		return ResponseEntity.ok(ApiResponse.success("항공기 목록 조회 성공", response));
 	}
 }
