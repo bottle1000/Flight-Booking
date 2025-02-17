281dc352dc5cba9a25723ac5bed63fbe4e2fd954
asdd2557 <asdd2557@naver.com>
2025-02-17 11:53:24 +0900

    feat: PreAuthorize어노테이션을 이용한 권한 검증 추가 및 짜잘한 UserID,Email 등등을 반환하는 로직을 User를 반환하는것으로 바꿈 Holder를 생성할때 DB에서 User객체를 가져오는 식으로 변경

--- src/main/java/flight_booking/demo/domain/user/controller/AdminController.java
+++ src/main/java/flight_booking/demo/domain/user/controller/AdminController.java
@@ -4,7 +4,10 @@ import flight_booking.demo.domain.user.dto.request.UpdateMemberShipRequestDto;
 import flight_booking.demo.domain.user.dto.request.UpdateRoleRequestDto;
 import flight_booking.demo.domain.user.entity.User;
 import flight_booking.demo.domain.user.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,16 +17,22 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AdminController {
     private final UserService userService;
+
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/")
     public List<User> findUsersAll(){
         return userService.findUserAll();
     }
+
+    @PreAuthorize("hasRole('ADMIN')")
     @PatchMapping("/role/{userId}")
-    public void updateRole(@RequestBody UpdateRoleRequestDto requestDto, @PathVariable String userId){
-        userService.updateRole(requestDto, userId);
+    public void updateRole(@RequestBody UpdateRoleRequestDto requestDto, @PathVariable String userId, HttpServletRequest request, HttpServletResponse response){
+        userService.updateRole(requestDto, userId,request,response);
     }
+
+    @PreAuthorize("hasRole('ADMIN')")
     @PatchMapping("/membership/{userId}")
-    public void updateMemberShip(@RequestBody UpdateMemberShipRequestDto requestDto, @PathVariable String userId) {
-        userService.updateMemberShip(requestDto,userId);
+    public void updateMemberShip(@RequestBody UpdateMemberShipRequestDto requestDto, @PathVariable String userId, HttpServletRequest request, HttpServletResponse response) {
+        userService.updateMemberShip(requestDto,userId,request,response);
     }
 }
--- src/main/java/flight_booking/demo/security/utils/UserUtil.java
+++ src/main/java/flight_booking/demo/security/utils/UserUtil.java
@@ -9,39 +9,8 @@ public class UserUtil {
     // 현재 인증된 사용자 정보를 가져오는 메서드
     public static User getCurrentUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        if (authentication == null || !authentication.isAuthenticated()) {
-            return null; //  로그인하지 않은 경우 null 반환
-        }
-
-        return (User) authentication.getPrincipal();
+        Object principal = authentication.getPrincipal();
+        return (User) principal;
     }
 
-    // 현재 로그인된 사용자의 ID 가져오기
-    public static String getCurrentUserId() {
-        User user = getCurrentUser();
-        return (user != null) ? user.getId() : null;
-    }
-
-    // 현재 로그인된 사용자의 이메일 가져오기
-    public static String getCurrentUserEmail() {
-        User user = getCurrentUser();
-        return (user != null) ? user.getEmail() : null;
-    }
-
-    // 현재 로그인된 사용자의 이름 가져오기
-    public static String getCurrentUserName() {
-        User user = getCurrentUser();
-        return (user != null) ? user.getName() : null;
-    }
-
-    // 현재 로그인된 사용자의 권한(Role) 가져오기
-    public static String getCurrentMemberShip() {
-        User user = getCurrentUser();
-        return (user != null) ? user.getMembership().name() : null;
-    }
-    public static String getCurrentRole() {
-        User user = getCurrentUser();
-        return (user != null) ? user.getRole().name() : null;
-    }
 }
