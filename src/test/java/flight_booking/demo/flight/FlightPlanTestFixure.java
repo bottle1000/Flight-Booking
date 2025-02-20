bd8b7001e49db1611017b4e502ffa81c5e9b7b81
sinwoo-kim <shinwoo5676@gmail.com>
2025-02-20 12:08:09 +0900

    feat: 통합 테스트 완료(Flight, Airplane)

--- src/test/java/flight_booking/demo/flight/FlightPlanTestFixure.java
+++ src/test/java/flight_booking/demo/flight/FlightPlanTestFixure.java
@@ -1,26 +1,26 @@
 package flight_booking.demo.flight;
 
-import static flight_booking.demo.domain.flight.entity.Airport.*;
-
-import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 import flight_booking.demo.domain.flight.dto.request.FlightPlanCreateRequest;
 import flight_booking.demo.domain.flight.dto.response.FlightPlanCreateResponse;
 import flight_booking.demo.domain.flight.entity.Airport;
 
 public class FlightPlanTestFixure {
-	// 상수 정의
-	public static final Airport departure = ICN;
-	public static final Airport arrival = GMP;
 
 	// 테스트용 요청 객체 생성 메서드
 	public static FlightPlanCreateRequest createFlightPlanRequest() {
 		return new FlightPlanCreateRequest(
 			"ICNGMP1922",
-			departure,
-			arrival,
-			LocalDateTime.of(2050, 1, 1, 11, 11, 11),
-			LocalDateTime.of(2055, 1, 1, 11, 11, 11),
+			Airport.ICN,
+			Airport.GMP,
+			ZonedDateTime.of(
+				2050, 3, 1, 1, 1, 1, 1, ZoneId.of("Asia/Seoul")
+			),
+			ZonedDateTime.of(
+				2051, 3, 1, 1, 1, 1, 1, ZoneId.of("Asia/Seoul")
+			),
 			30000
 		);
 	}
@@ -30,10 +30,14 @@ public class FlightPlanTestFixure {
 		return new FlightPlanCreateResponse(
 			1L,
 			"ICNGMP1922",
-			departure,
-			arrival,
-			LocalDateTime.of(2050, 1, 1, 11, 11, 11),
-			LocalDateTime.of(2055, 1, 1, 11, 11, 11),
+			Airport.ICN,
+			Airport.GMP,
+			ZonedDateTime.of(
+				2050, 3, 1, 1, 1, 1, 0, ZoneId.of("Asia/Seoul")
+			),
+			ZonedDateTime.of(
+				2051, 3, 1, 1, 1, 1, 0, ZoneId.of("Asia/Seoul")
+			),
 			30000,
 			"ICNGMP1922"
 		);
--- src/test/java/flight_booking/demo/flight/controller/FlightPlanControllerTest.java
+++ src/test/java/flight_booking/demo/flight/controller/FlightPlanControllerTest.java
@@ -1,32 +1,25 @@
 package flight_booking.demo.flight.controller;
 
-import static flight_booking.demo.flight.FlightPlanTestFixure.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-
-
-import com.fasterxml.jackson.databind.ObjectMapper;  // Jackson의 ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.util.StdDateFormat;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import flight_booking.demo.BaseTest;
 import flight_booking.demo.domain.airplane.entity.Airplane;
@@ -37,12 +30,10 @@ import flight_booking.demo.domain.flight.entity.FlightPlan;
 import flight_booking.demo.domain.flight.entity.Ticket;
 import flight_booking.demo.domain.flight.repository.FlightPlanRepository;
 import flight_booking.demo.domain.flight.repository.TicketRepository;
-import flight_booking.demo.domain.order.entity.Order;
 import flight_booking.demo.domain.user.entity.Role;
 import flight_booking.demo.domain.user.entity.User;
 import flight_booking.demo.domain.user.repository.UserRepository;
 import flight_booking.demo.flight.FlightPlanTestFixure;
-import flight_booking.demo.security.jwt.TokenProvider;
 import flight_booking.demo.security.utils.UserUtil;
 import mockuser.WithMockUser;
 
@@ -53,10 +44,8 @@ public class FlightPlanControllerTest extends BaseTest {
 
 	@Autowired
 	private MockMvc mockMvc;
-
-	// 필드 선언
-	private final ObjectMapper objectMapper = new ObjectMapper();
-
+	@Autowired
+	private ObjectMapper objectMapper;
 	@Autowired
 	private TicketRepository ticketRepository;
 	@Autowired
@@ -68,91 +57,91 @@ public class FlightPlanControllerTest extends BaseTest {
 
 	private Airplane airplane;
 	private FlightPlan flightPlan;
-	private Ticket ticket;
-	private Order order;
-	private TokenProvider tokenProvider;
-
-	@Autowired
-	private RestTemplate restTemplate;
 
 	@BeforeEach
 	void setUp() {
 		airplane = Airplane.from("test airplane");
-		airplaneRepository.save(airplane);
+		airplane = airplaneRepository.save(airplane);
 
 		User userA = UserUtil.getCurrentUser();
 		userA.setRole(Role.ADMIN);
 		userRepository.save(userA);
 
 		flightPlan = FlightPlan.create(
-			"ICNNRT",
+			"ICNGMP1922",
 			Airport.ICN,
 			Airport.NRT,
 			10000,
-			LocalDateTime.of(2050, 1, 1, 11, 11, 11).atZone(zoneId),
-			LocalDateTime.of(2055, 1, 1, 11, 11, 11).atZone(zoneId),
+			ZonedDateTime.of(
+				2050, 3, 1, 1, 1, 1, 1, ZoneId.of("Asia/Seoul")
+			),
+			ZonedDateTime.of(
+				2051, 3, 1, 1, 1, 1, 1, ZoneId.of("Asia/Seoul")
+			),
 			airplane);
 		flightPlanRepository.save(flightPlan);
 
 		Ticket ticket = new Ticket("1A", flightPlan);
 		ticketRepository.save(ticket);
-
-		// LocalDateTime 직렬화 및 역직렬화 로직
-		objectMapper.registerModule(new JavaTimeModule());
-		objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 	}
 
 	@Test
-	@DisplayName("항공권 일정 생성 통합 테스트")
+	@DisplayName("항공권 일정 생성 통합 테스트 - 성공")
 	public void createFlightPlan() throws Exception {
 
+		Long airplaneId = airplane.getId();
 		FlightPlanCreateRequest request = FlightPlanTestFixure.createFlightPlanRequest();
-		mockMvc.perform(MockMvcRequestBuilders.post("/admin/airplanes/1/flight-plans")
-			.contentType(MediaType.APPLICATION_JSON)
-			.content(objectMapper.writeValueAsString(request))
-		).andExpect(status().isOk())
+		String requestJson = objectMapper.writeValueAsString(request);
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/admin/airplanes/" + airplaneId + "/flight-plans")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(requestJson)
+			).andExpect(status().isOk())
 			.andExpect(jsonPath("$.name").value(request.name()))
-			// .andExpect(jsonPath("$.departure").value(request.departure()))
-			// .andExpect(jsonPath("$.arrival").value(request.arrival()))
-			.andExpect(jsonPath("$.boardingAt").value(request.boardingAt()))
-			.andExpect(jsonPath("$.landingAt").value(request.landingAt()))
-			.andExpect(jsonPath("$.price").value(request.price()));
+			.andReturn();
+
+		String responseBody = result.getResponse().getContentAsString();
+		System.out.println("Response: " + responseBody);
 	}
 
-
 	@Test
-	@DisplayName("항공권 일정 검색 통합 테스트")
-	void findFlightPlanPage() {
-		//given
+	@DisplayName("항공권 일정 검색 통합 테스트 - 성공")
+	public void findFlightPlanPage() throws Exception {
 		String departure = "ICN";
-		String arrival = "GMP";
-		LocalDate fromDate = LocalDate.of(2025, 3, 1);
-		LocalDate toDate = LocalDate.of(2025, 3, 15);
-		int page = 0;
-		int size = 10;
+		String arrival = "NRT";
+		LocalDate fromDate = LocalDate.of(2050, 3, 1);
+		String fromDateString = fromDate.toString();
+		LocalDate toDate = LocalDate.of(2051, 3, 1);
+		String toDateString = toDate.toString();
 
-		//when
-		String url = UriComponentsBuilder.fromHttpUrl("http://localhost:8080/flight-plans")
-			.queryParam("departure", departure)
-			.queryParam("arrival", arrival)
-			.queryParam("fromDate", fromDate)
-			.queryParam("toDate", toDate)
-			.toUriString();
+		MvcResult result = mockMvc.perform(get("/flight-plans")
+				.param("departure", departure)
+				.param("arrival", arrival)
+				.param("fromDate", fromDateString)
+				.param("toDate", toDateString))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.content[0].departure").value(departure))
+			.andExpect(jsonPath("$.content[0].arrival").value(arrival))
+			.andExpect(jsonPath("$.content[0].boardingAt").value(containsString(fromDateString)))
+			.andExpect(jsonPath("$.content[0].landingAt").value(containsString(toDateString)))
+			.andExpect(jsonPath("$.totalCount").value(1))
+			.andReturn();
 
-		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-
-		//then
-		assertEquals(HttpStatus.OK, response.getStatusCode());
-		assertTrue(response.getBody().contains("content"));
+		String responseBody = result.getResponse().getContentAsString();
+		System.out.println("Response: " + responseBody);
 	}
 
 	@Test
-	void findFlightPlan() {
+	@DisplayName("항공권 일정 단건 조회 통합 테스트 - 성공")
+	public void findFlightPlan() throws Exception {
+		Long flightPlanId = flightPlan.getId();
+		MvcResult result = mockMvc.perform(get("/flight-plans/" + flightPlanId))
+			.andExpect(status().isOk())
+			// 티켓 안의 flightPlan 정보 검증
+			.andExpect(jsonPath("$[0].ticketList[0].flightPlan.name").value(flightPlan.getName()))
+			.andReturn();
 
-	}
-
-	@Test
-	void updateFlightPlan() {
+		String responseBody = result.getResponse().getContentAsString();
+		System.out.println("Response: " + responseBody);
 	}
 
 }
