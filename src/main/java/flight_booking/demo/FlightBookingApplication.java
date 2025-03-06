e12c7b91e5fbc4b42d815811d9f30d59ec669ef9
박병천 <genius178896@gmail.com>
2025-03-06 09:17:29 +0900

    feat : 스케쥴링 추가

--- src/main/java/flight_booking/demo/FlightBookingApplication.java
+++ src/main/java/flight_booking/demo/FlightBookingApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.Properties;
 
@@ -13,6 +14,7 @@ import java.util.Properties;
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableRetry
+@EnableScheduling
 public class FlightBookingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FlightBookingApplication.class, args);
