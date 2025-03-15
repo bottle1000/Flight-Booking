79a514913f9167b41b60a771ee42169fd643732c
sinwoo-kim <shinwoo5676@gmail.com>
2025-03-15 20:06:01 +0900

    feat: 테스트를 위해 redis pool size 제한

--- src/main/java/flight_booking/demo/config/RedissonConfig.java
+++ src/main/java/flight_booking/demo/config/RedissonConfig.java
@@ -4,6 +4,7 @@ import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,10 +20,16 @@ public class RedissonConfig {
     private static final String REDISSON_HOST_PREFIX = "redis://";
 
     @Bean
+    @ConditionalOnProperty(name = "spring.redis.enabled", havingValue = "true", matchIfMissing = true)
     public RedissonClient redissonClient() {
         Config config = new Config();
+        // Netty 스레드 설정은 Config 객체에 직접 적용
+        config.setNettyThreads(2);  // 여기에 설정
+
         config.useSingleServer()
-                .setAddress(REDISSON_HOST_PREFIX + redisHost + ":" + redisPort);
+            .setAddress(REDISSON_HOST_PREFIX + redisHost + ":" + redisPort)
+            .setConnectionMinimumIdleSize(1)
+            .setConnectionPoolSize(2);
         return Redisson.create(config);
     }
 }
